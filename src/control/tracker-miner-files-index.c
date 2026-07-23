GNOME/localsearch

commit e0e329333e8195dc624f9ae9ea8c9d113989d036

control: Use g_autoptr further

src/control/tracker-miner-files-index.c
@@ -33,6 +33,10 @@
  */
 #undef REQUIRE_LOCATION_IN_CONFIG
 
+struct _TrackerMinerFilesIndex {
+	GObject parent_instance;
+};
+
 typedef struct {
 	TrackerMinerFilesPeerListener *peer_listener;
 	TrackerDBusMinerFilesIndex *skeleton;
@@ -117,10 +121,7 @@ index_finalize (GObject *object)
 	g_object_unref (priv->skeleton);
 	g_object_unref (priv->proxy_skeleton);
 
-	if (priv->d_connection) {
-		g_object_unref (priv->d_connection);
-	}
-
+	g_clear_object (&priv->d_connection);
 	g_clear_object (&priv->peer_listener);
 	g_array_unref (priv->indexed_files);
 	g_free (priv->full_path);
@@ -175,7 +176,7 @@ tracker_miner_files_index_handle_index_location (TrackerDBusMinerFilesIndex *ske
 	TrackerMinerFilesIndexPrivate *priv;
 	TrackerDBusRequest *request;
 	TrackerIndexLocationFlags index_flags;
-	GFile *file;
+	g_autoptr (GFile) file = NULL;
 
 	priv = TRACKER_MINER_FILES_INDEX_GET_PRIVATE (index);
 
@@ -198,8 +199,6 @@ tracker_miner_files_index_handle_index_location (TrackerDBusMinerFilesIndex *ske
 	tracker_dbus_request_end (request, NULL);
 	g_dbus_method_invocation_return_value (invocation, NULL);
 
-	g_object_unref (file);
-
 	return TRUE;
 }
 
@@ -210,7 +209,7 @@ peer_listener_unwatch_file (TrackerMinerFilesPeerListener *listener,
 {
 	TrackerMinerFilesIndex *index = user_data;
 	TrackerMinerFilesIndexPrivate *priv = TRACKER_MINER_FILES_INDEX_GET_PRIVATE (index);
-	gchar *uri;
+	g_autofree char *uri = NULL;
 	gint i;
 
 	uri = g_file_get_uri (file);
@@ -274,23 +273,21 @@ tracker_miner_files_index_init (TrackerMinerFilesIndex *object)
 TrackerMinerFilesIndex *
 tracker_miner_files_index_new (void)
 {
-	GObject *miner;
+	g_autoptr (TrackerMinerFilesIndex) files_index = NULL;
 	TrackerMinerFilesIndexPrivate *priv;
-	gchar *full_path;
-	GError *error = NULL;
+	g_autofree char *full_path = NULL;
+	g_autoptr (GError) error = NULL;
 
-	miner = g_object_new (TRACKER_TYPE_MINER_FILES_INDEX,
-	                      NULL);
+	files_index = g_object_new (TRACKER_TYPE_MINER_FILES_INDEX,
+	                            NULL);
 
-	priv = TRACKER_MINER_FILES_INDEX_GET_PRIVATE (miner);
+	priv = TRACKER_MINER_FILES_INDEX_GET_PRIVATE (files_index);
 
 	priv->d_connection = g_bus_get_sync (G_BUS_TYPE_SESSION, NULL, &error);
 
 	if (!priv->d_connection) {
 		g_critical ("Could not connect to the D-Bus session bus, %s",
 		            error ? error->message : "no error given.");
-		g_clear_error (&error);
-		g_object_unref (miner);
 		return NULL;
 	}
 
@@ -299,7 +296,7 @@ tracker_miner_files_index_new (void)
 
 	g_debug ("Registering D-Bus object...");
 	g_debug ("  Path:'%s'", full_path);
-	g_debug ("  Object Type:'%s'", G_OBJECT_TYPE_NAME (miner));
+	g_debug ("  Object Type:'%s'", G_OBJECT_TYPE_NAME (files_index));
 
 	if (!g_dbus_interface_skeleton_export (G_DBUS_INTERFACE_SKELETON (priv->skeleton),
 	                                       priv->d_connection,
@@ -308,8 +305,6 @@ tracker_miner_files_index_new (void)
 		g_critical ("Could not register the D-Bus object %s, %s",
 		            full_path,
 		            error ? error->message : "no error given.");
-		g_clear_error (&error);
-		g_object_unref (miner);
 		return NULL;
 	}
 
@@ -320,8 +315,6 @@ tracker_miner_files_index_new (void)
 		g_critical ("Could not register the D-Bus object %s, %s",
 		            TRACKER_MINER_DBUS_NAME_PREFIX "Files.Proxy",
 		            error ? error->message : "no error given.");
-		g_clear_error (&error);
-		g_object_unref (miner);
 		return NULL;
 	}
 
@@ -329,9 +322,9 @@ tracker_miner_files_index_new (void)
 
 	priv->peer_listener = tracker_miner_files_peer_listener_new (priv->d_connection);
 	g_signal_connect (priv->peer_listener, "unwatch-file",
-	                  G_CALLBACK (peer_listener_unwatch_file), miner);
+	                  G_CALLBACK (peer_listener_unwatch_file), files_index);
 	g_signal_connect (priv->peer_listener, "graphs-changed",
-	                  G_CALLBACK (peer_listener_graphs_changed), miner);
+	                  G_CALLBACK (peer_listener_graphs_changed), files_index);
 
-	return (TrackerMinerFilesIndex *) miner;
+	return g_steal_pointer (&files_index);
 }

src/control/tracker-main.c
@@ -114,10 +114,10 @@ files_index_close_cb (TrackerMinerFilesIndex *index,
 int
 main (gint argc, gchar *argv[])
 {
-	GOptionContext *context;
-	GError *error = NULL;
-	GDBusConnection *connection;
-	TrackerMinerFilesIndex *index;
+	g_autoptr (GOptionContext) context = NULL;
+	g_autoptr (GError) error = NULL;
+	g_autoptr (GDBusConnection) connection = NULL;
+	g_autoptr (TrackerMinerFilesIndex) index = NULL;
 
 	main_loop = NULL;
 
@@ -137,11 +137,9 @@ main (gint argc, gchar *argv[])
 
 	g_option_context_add_main_entries (context, entries, NULL);
 	g_option_context_parse (context, &argc, &argv, &error);
-	g_option_context_free (context);
 
 	if (error) {
 		g_printerr ("%s\n", error->message);
-		g_error_free (error);
 		return EXIT_FAILURE;
 	}
 
@@ -154,7 +152,6 @@ main (gint argc, gchar *argv[])
 	if (error) {
 		g_critical ("Could not create DBus connection: %s\n",
 		            error->message);
-		g_error_free (error);
 		return EXIT_FAILURE;
 	}
 
@@ -170,7 +167,6 @@ main (gint argc, gchar *argv[])
 	                                &error)) {
 		g_critical ("Could not request DBus name: %s",
 		            error->message);
-		g_error_free (error);
 		return EXIT_FAILURE;
 	}
 
@@ -179,23 +175,16 @@ main (gint argc, gchar *argv[])
 	                                &error)) {
 		g_critical ("Could not request DBus name: %s",
 		            error->message);
-		g_error_free (error);
 		return EXIT_FAILURE;
 	}
 
 	initialize_signal_handler ();
 
-	/* Go, go, go! */
 	g_main_loop_run (main_loop);
 
 	g_debug ("Shutdown started");
 
 	g_main_loop_unref (main_loop);
-
-	g_object_unref (connection);
-	g_object_unref (index);
-
-	g_print ("\nOK\n\n");
 
 	return EXIT_SUCCESS;
 }

src/control/tracker-miner-files-index.h
@@ -24,26 +24,13 @@
 
 G_BEGIN_DECLS
 
-#define TRACKER_TYPE_MINER_FILES_INDEX            (tracker_miner_files_index_get_type ())
-#define TRACKER_MINER_FILES_INDEX(object)         (G_TYPE_CHECK_INSTANCE_CAST ((object), TRACKER_TYPE_MINER_FILES_INDEX, TrackerMinerFilesIndex))
-#define TRACKER_MINER_FILES_INDEX_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), TRACKER_TYPE_DBUS_MINER_FILES_INDEX, TrackerMinerFilesIndexClass))
-#define TRACKER_IS_MINER_FILES_INDEX(object)      (G_TYPE_CHECK_INSTANCE_TYPE ((object), TRACKER_TYPE_MINER_FILES_INDEX))
-#define TRACKER_IS_MINER_FILES_INDEX_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), TRACKER_TYPE_MINER_FILES_INDEX))
-#define TRACKER_MINER_FILES_INDEX_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), TRACKER_TYPE_MINER_FILES_INDEX, TrackerMinerFilesIndexClass))
+#define TRACKER_TYPE_MINER_FILES_INDEX (tracker_miner_files_index_get_type ())
+G_DECLARE_FINAL_TYPE (TrackerMinerFilesIndex,
+                      tracker_miner_files_index,
+                      TRACKER, MINER_FILES_INDEX,
+                      GObject)
 
-typedef struct TrackerMinerFilesIndex TrackerMinerFilesIndex;
-typedef struct TrackerMinerFilesIndexClass TrackerMinerFilesIndexClass;
-
-struct TrackerMinerFilesIndex {
-	GObject parent;
-};
-
-struct TrackerMinerFilesIndexClass {
-	GObjectClass parent;
-};
-
-GType                   tracker_miner_files_index_get_type (void);
-TrackerMinerFilesIndex *tracker_miner_files_index_new      (void);
+TrackerMinerFilesIndex *tracker_miner_files_index_new (void);
 
 G_END_DECLS
 

src/control/tracker-miner-files-peer-listener.c
@@ -289,13 +289,13 @@ unwatch_file (TrackerMinerFilesPeerListener *listener,
               GFile                         *file)
 {
 	TrackerMinerFilesPeerListenerPrivate *priv;
+	g_autoptr (GFile) f = NULL;
 
 	priv = tracker_miner_files_peer_listener_get_instance_private (listener);
 
-	g_object_ref (file);
-	g_hash_table_remove (priv->file_peers, file);
-	g_signal_emit (listener, signals[UNWATCH_FILE], 0, file);
-	g_object_unref (file);
+	f = g_object_ref (file);
+	g_hash_table_remove (priv->file_peers, f);
+	g_signal_emit (listener, signals[UNWATCH_FILE], 0, f);
 }
 
 static void

commit a5302b60ca3600cd50585cdf5888b05b131336f4

control: Fix double free on shutdown
The variable where the full D-Bus path was made g_autofree, but its
ownership was not transferred when keeping it around.

Fixes: e0e329333 ("control: Use g_autoptr further")

src/control/tracker-miner-files-index.c
@@ -227,7 +227,7 @@ tracker_miner_files_index_initable_init (GInitable     *initable,
 	                                       error))
 		return FALSE;
 
-	files_index->full_path = full_path;
+	files_index->full_path = g_steal_pointer (&full_path);
 
 	files_index->peer_listener = tracker_miner_files_peer_listener_new (files_index->d_connection);
 	g_signal_connect (files_index->peer_listener, "unwatch-file",